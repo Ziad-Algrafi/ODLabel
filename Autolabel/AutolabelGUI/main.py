24f7c404ba95b93724ad20e9cb343a977fb35149
Ziad <117011801+Ziad-Algrafi@users.noreply.github.com>
2024-03-31 15:45:36 +0300

    Delete Autolabel/AutolabelGUI/main.py

--- Autolabel/AutolabelGUI/main.py
+++
@@ -1,9 +0,0 @@
-import sys
-from PyQt6.QtWidgets import QApplication
-from main_window import MainWindow
-
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    window = MainWindow()
-    window.show()
-    sys.exit(app.exec())
