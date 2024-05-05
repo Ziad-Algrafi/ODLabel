f711b928de1d63b8b789658a6bf9af6bfb25fec2
Ziad <117011801+Ziad-Algrafi@users.noreply.github.com>
2024-05-05 22:31:22 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="odlabel",
-    version="0.7.19",
+    version="0.7.20",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
