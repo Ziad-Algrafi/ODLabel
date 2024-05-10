7c310385ee7faad7dd3f3c8d6d049f16e999895c
Ziad <ziadalgrafi@gmail.com>
2024-05-10 19:18:54 +0300

    Update draw_utils.py

--- app/workers/utils/draw_utils.py
+++ app/workers/utils/draw_utils.py
@@ -1,13 +1,9 @@
 import cv2 as cv
-import os
+import random
 
-
-def draw_boxes_on_image(annotated_frame, detections, img_path, output_path=None):
-    if output_path is None:
-        output_path = os.path.splitext(img_path)[0] + "_with_boxes.jpg"
-
+def draw_boxes_on_image(annotated_frame, detections, class_colors, output_path):
     for (
-        _,
+        cls_id,
         x_scaled,
         y_scaled,
         w_scaled,
@@ -20,19 +16,52 @@ def draw_boxes_on_image(annotated_frame, detections, img_path, output_path=None)
         y1 = int(y_scaled - h_scaled / 2)
         x2 = int(x_scaled + w_scaled / 2)
         y2 = int(y_scaled + h_scaled / 2)
-        label = class_name
-        cv.rectangle(annotated_frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+
+        if cls_id not in class_colors:
+            class_colors[cls_id] = (
+                random.randint(0, 255),
+                random.randint(0, 255),
+                random.randint(0, 255),
+            )
+
+        color = class_colors[cls_id]
+
+        img_height, img_width = annotated_frame.shape[:2]
+        
+        object_width = x2 - x1
+        object_height = y2 - y1
+        size_ratio = min(object_width, object_height) / min(img_width, img_height)
+        font_scale = min(0.8, 0.4 + size_ratio * 0.6)
+        text_thickness = max(1, int(font_scale * 2))
+
+        cv.rectangle(annotated_frame, (x1, y1), (x2, y2), color, 2)
+
+        caption = f"{class_name} {confidence*100:.2f}%"
+        (tw, th), _ = cv.getTextSize(
+            text=caption,
+            fontFace=cv.FONT_HERSHEY_SIMPLEX,
+            fontScale=font_scale,
+            thickness=text_thickness,
+        )
+        th = int(th * 1.2)
+        tw = int(tw * 1.1)
+
+        bg_x1 = max(x1, 0)
+        bg_y1 = max(y1 - th, 0)
+        bg_x2 = min(x1 + tw, img_width)
+        bg_y2 = y1
+
+        cv.rectangle(annotated_frame, (bg_x1, bg_y1), (bg_x2, bg_y2), color, -1)
         cv.putText(
             annotated_frame,
-            label,
-            (x1, y1 - 5),
+            caption,
+            (x1, y1),
             cv.FONT_HERSHEY_SIMPLEX,
-            0.5,
-            (0, 255, 0),
-            2,
+            font_scale,
+            (255, 255, 255),
+            text_thickness,
+            cv.LINE_AA,
         )
-        # cv.putText(annotated_frame, f" {confidence:.2f}", (x1 + 20, y1 - 5), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
     cv.imwrite(output_path, annotated_frame)
-
     return annotated_frame
